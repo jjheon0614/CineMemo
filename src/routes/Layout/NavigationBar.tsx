f30bcba763a878621a33cd06c0166f5215fe2223
jaeheon <jjheon0614@gmail.com>
2025-01-16 20:21:36 +0700

    create Login, Signup

--- src/routes/Layout/NavigationBar.tsx
+++ src/routes/Layout/NavigationBar.tsx
@@ -1,4 +1,5 @@
 import { Div } from "../../components"
+import { Link } from "react-router-dom"
 
 export default function NavigationBar() {
     return (
@@ -14,6 +15,14 @@ export default function NavigationBar() {
             <p className="text-xl font-bold">
                 Profile
             </p>
+            <div className="flex items-center p-2">
+                <Link to="/login" className="btn btn-sm btn-primary">
+                    Login
+                </Link>
+                <Link to="/signup" className="ml-4 btn btn-sm btn-outline btn-primary">
+                    Signup
+                </Link>
+            </div>
         </Div>
     )
 }
--- src/routes/RoutesSetup.tsx
+++ src/routes/RoutesSetup.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import Layout from "./Layout";
 import NoMatch from "./NoMatch";
+import Login from "./Auth/Login";
+import Signup from './Auth/Signup'
 
 export default function RoutesSetup() {
     return (
@@ -10,6 +12,8 @@ export default function RoutesSetup() {
                 <Route index element={<LandingPage />} />
                 <Route path="*" element={<NoMatch />} />
             </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="*" element={<NoMatch />} />
         </Routes>
     )
