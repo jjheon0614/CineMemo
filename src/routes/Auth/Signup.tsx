534e6461cd46c3fa0ce495f643c4bacc12f6c491
jaeheon <jjheon0614@gmail.com>
2025-01-16 20:27:10 +0700

    Create Signup page

--- src/routes/Auth/Signup.tsx
+++ src/routes/Auth/Signup.tsx
@@ -1,8 +1,43 @@
+import { Link } from "react-router-dom"
+
 export default function Signup() {
     return (
-      <section className="mt-4">
-        <h2 className="text-5xl font-bold text-center">Signup</h2>
-        <div className="mt-4"></div>
-      </section>
+      <div className="flex flex-col min-h-screen bg-gray-100 border border-gray-300 shadow-xl rounded-xl">
+            <div className="flex flex-col items-center justify-center flex-1 max-w-sm px-2 mx-auto">
+                <div className="w-full px-6 py-8 text-black bg-white rounded shadow-md">
+                    <h1 className="mb-8 text-2xl text-center text-primary">Sign Up</h1>
+                    <input 
+                        type="text"
+                        className="w-full p-3 mb-4 bg-gray-100 input input-primary"
+                        name="email"
+                        placeholder="Email"
+                    />
+                    <input
+                        type="password"
+                        className="w-full p-3 mb-4 bg-gray-100 input input-primary"
+                        name="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        type="password"
+                        className="w-full p-3 mb-4 bg-gray-100 input input-primary"
+                        name="confirm_password"
+                        placeholder="Confirm Password"
+                    />
+                    <button
+                        type="submit"
+                        className="w-full btn btn-primary">
+                        Create Account
+                    </button>
+                </div>
+
+                <div className="mt-6 text-grey-dark">
+                    Already have an account?
+                    <Link className="btn btn-link btn-primary" to="/login">
+                        Login
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
   }
